ca66c8d372d7f4c8253000ca838eadc2e022ebaf
kotyb <70448027+kotyb@users.noreply.github.com>
2020-10-10 18:29:45 -0400

    Add files via upload

--- BuyStockCommand.java
+++ BuyStockCommand.java
@@ -1,18 +1,19 @@
-//Team Victor
+//Koty B
 public class BuyStockCommand implements Command {
 	
 	private StockTrade stockTrade;
 	private String stockName;
 	private int shares;
 	
+	//initializes variables
 	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares) {
 		this.stockTrade = stockTrade;
 		this.stockName = stockName;
 		this.shares = shares;
 	}
 	
+	//executes the command buy
 	public void execute() {
-		// depends on command name
-		//	BuyStockCommand.buy();
+		stockTrade.buy(stockName, shares);
 	}
 }
--- SellStockCommand.java
+++ SellStockCommand.java
@@ -1,18 +1,19 @@
-//Team Victor
+//Koty B
 public class SellStockCommand implements Command {
 
 	private StockTrade stockTrade;
 	private String stockName;
 	private int shares;
 	
+	//initializes variables
 	public SellStockCommand(StockTrade stockTrade, String stockName, int shares) {
 		this.stockTrade = stockTrade;
 		this.stockName = stockName;
 		this.shares = shares;
 	}
 	
+	//executes the command sell
 	public void execute() {
-		//depends on the command names
-		//SellStockCommand.sell();
+		stockTrade.sell(stockName, shares);
 	}
 }

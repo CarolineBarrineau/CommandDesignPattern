54c940986e1dff7e9d5c5dfd42301f85b5df97b8
Caroline Barrineau <caroline.barrineau@gmail.com>
2020-10-10 16:20:57 -0400

    Add files via upload

---
+++ StockTrade.java
@@ -0,0 +1,13 @@
+//Team Victor
+public class StockTrade {
+	
+	public StockTrade() {}
+
+	public void buy(String stockName, int numShares) {
+		System.out.println("Buying " + numShares + " of " + stockName);
+	}
+
+	public void sell(String stockName, int numShares) {
+		System.out.println("Selling " + numShares + " of " + stockName);
+	}
+}

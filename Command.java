647b254827a414fde39bd2cee5823b87a120c0a7
unknown <daleman@email.sc.edu>
2020-10-10 17:13:01 -0400

    added command interface

---
+++ Command.java
@@ -0,0 +1,13 @@
+/**
+ * This is an interface that contains the execute method
+ * @author daleman123
+ *
+ */
+public interface Command {
+	
+	/**
+	 * Method used in the BuyStockCommand and SellStockCommand classes
+	 */
+	public void execute();
+	
+}
